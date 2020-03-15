e4656ee8ca60c3a78d2ba851ac6432d1758fd307
rnavickas <r.navickas@stud.vgtu.lt>
2020-03-15 13:02:21 +0200

    Created dist folder for deployment

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+app.use(express.static(__dirname + '/dist/terai-frontend'));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<terai-frontend>'));
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/<terai-frontend>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/terai-frontend/index.html'));
+});
