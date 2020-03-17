07e8cec474b55c9d0758d70cf2e2735cafded31c
alinaser <alina.paplinska@gmail.com>
2020-03-17 13:37:49 +0200

    new commit

---
+++ src/app/home-page/listOfItems.ts
@@ -0,0 +1,13 @@
+import {Copyright} from '../copyright';
+
+
+export const copyrigthList: Copyright[] = [
+    {
+        type: "photo", 
+        year: 2018, 
+        author : "Shridhar Gupta", 
+        activeLink: "https://unsplash.com/photos/dZxQn4VEv2M",
+        platform : "Unsplash"
+    }
+    
+];
