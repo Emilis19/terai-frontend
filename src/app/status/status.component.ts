184e8487bad0ae5308102f4cad625019d0dbe191
alinaser <alina.paplinska@gmail.com>
2020-04-09 13:52:45 +0300

    fix

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -24,27 +24,27 @@ export class StatusComponent implements OnInit {
   ngOnInit(): void {
     switch (this.applicationReviewContent.status) {
       case this.statusEnum.Gavo:
-        this.divClass = "col-sm-12 alert purple pt-4 pb-4 pl-5 pr-5";
+        this.divClass = "col-12 alert purple pt-4 pb-4 pl-5 pr-5";
         this.imageSrc = "assets/img/receive-mail.svg";
         this.answer = 1;
         break;
       case this.statusEnum.Priimtas:
-        this.divClass = "col-sm-12 alert green pt-4 pb-4 pl-5 pr-5";
+        this.divClass = "col-12 alert green pt-4 pb-4 pl-5 pr-5";
         this.imageSrc = "assets/img/tick-inside-circle.svg";
         this.answer = 2;
         break;
       case this.statusEnum.Nepriime:
-        this.divClass = "col-sm-12 alert redN pt-4 pb-4 pl-5 pr-5";
+        this.divClass = "col-12 alert redN pt-4 pb-4 pl-5 pr-5";
         this.imageSrc = "assets/img/cancel.svg";
         this.answer = 3;
         break;
       case this.statusEnum.Nebaigta:
-        this.divClass = "col-sm-12 alert white pt-4 pb-4 pl-5 pr-5";
+        this.divClass = "col-12 alert white pt-4 pb-4 pl-5 pr-5";
         this.imageSrc = "assets/img/incomplete-hand-drawn-symbol.svg";
         this.answer = 4;
         break;
       case this.statusEnum.Vykdoma:
-        this.divClass = "col-sm-12 alert orangle pt-4 pb-4 pl-5 pr-5";
+        this.divClass = "col-12 alert orangle pt-4 pb-4 pl-5 pr-5";
         this.imageSrc = "assets/img/hourglass.svg";
         this.answer = 5;
         break;
